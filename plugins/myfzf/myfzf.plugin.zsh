6c7f428e8f7e0f818f2a09624253252733288425
waterschen <qinshui.c@gmail.com>
2019-09-12 17:23:39 +0800

    `fo`: press Enter to open with `open` command
    Signed-off-by: waterschen <qinshui.c@gmail.com>

--- plugins/myfzf/myfzf.plugin.zsh
+++ plugins/myfzf/myfzf.plugin.zsh
@@ -46,15 +46,15 @@ fe() {
 }
 
 # Modified version where you can press
-#   - CTRL-O to open with `open` command,
-#   - CTRL-E or Enter key to open with the $EDITOR
+#   - CTRL-O or Enter to open with `open` command,
+#   - CTRL-E to open with the $EDITOR
 fo() {
     local out file key
     IFS=$'\n' out=("$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)")
     key=$(head -1 <<< "$out")
     file=$(head -2 <<< "$out" | tail -1)
     if [ -n "$file" ]; then
-        [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+        [ "$key" = ctrl-e ] && ${EDITOR:-vim} "$file" || open "$file"
     fi
 }
 
