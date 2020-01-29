6a48e4c2cbd0fbf8ba86aafc8d94193857e8aa68
waterschen <qinshui.c@gmail.com>
2020-01-29 21:02:33 +0800

    add vagrant ussh alias
    Signed-off-by: waterschen <qinshui.c@gmail.com>

--- plugins/myvagrant/myvagrant.plugin.zsh
+++ plugins/myvagrant/myvagrant.plugin.zsh
@@ -1,11 +1,11 @@
 # aliases
 alias vup='vagrant up'
 alias vh='vagrant halt'
-# alias vssh='vagrant ssh'
+alias vssh='vagrant ssh'
 alias vdstr='vagrant destroy'
 
 # ssh into vagrant devbox
-function vssh() {
+function ussh() {
     cd $HOME/Developer/vagrant
     if [[ $(vagrant status) =~ "running" ]]; then
         vagrant ssh
